6e62b3f1636adc89013c3aa89a12f379e55ec63d
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-09-11 01:29:23 -0400

    Initial file structure

---
+++ config/config.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+module.exports = {
+  development: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "database_development",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  test: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  },
+  production: {
+    username: "root",
+    password: process.env.PASSWORD,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+    operatorsAliases: false
+  }
+};
