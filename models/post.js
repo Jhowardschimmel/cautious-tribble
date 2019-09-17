b1aa64c21733e503a2a1cc1660865599f226a573
Jesse Schimmel <jhowardschimmel@gmail.com>
2019-09-17 18:48:04 -0400

    add post.js model

--- models/post.js
+++ models/post.js
@@ -5,3 +5,21 @@ module.exports = function(sequelize, DataTypes) {
   });
   return Post;
 };
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    body: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      len: [1]
+    }
+  });
+
+  return Post;
+};
